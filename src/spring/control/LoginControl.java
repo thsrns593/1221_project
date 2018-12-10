60ef98e7a9cecedbc9649202a02418330e176a28
ydw7963 <45552218+ydw7963@users.noreply.github.com>
2018-12-10 21:33:22 +0900

    Update .classpath

--- src/spring/control/LoginControl.java
+++ src/spring/control/LoginControl.java
@@ -1,13 +1,42 @@
 package spring.control;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.AlternativeJdkIdGenerator;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import mybatis.dao.LoginDAO;
 
 @Controller
 public class LoginControl {
-
-	@RequestMapping("login.inc")
+	
+	@Autowired 
+	private LoginDAO l_dao;
+	@Autowired
+	private HttpSession session;
+	@RequestMapping("/login.inc")
 	public String login() {
 		return "login";
 	}
+	
+	@RequestMapping(value="/login.inc", method=RequestMethod.POST)
+	public ModelAndView login(String email, String password) {
+		ModelAndView mv = new ModelAndView();
+		
+		if(l_dao.login(email, password) != null) {
+			session.setAttribute("id", email);
+			mv.setViewName("main");
+		}else {
+			
+			mv.setViewName("redirect:/login.inc?check=fail");
+		}
+		return mv;
+	}
+	
+	
 }
