bb3a2362b2b51e7923f939de24df9329091f6f3e
psajo <psajo@naver.com>
2018-12-10 20:02:03 +0900

    aa

--- src/spring/control/Text_ReadControl.java
+++ src/spring/control/Text_ReadControl.java
@@ -1,13 +1,24 @@
 package spring.control;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mybatis.dao.NormalDAO;
+
 @Controller
 public class Text_ReadControl {
-
+	@Autowired
+	NormalDAO n_dao;
+	
 	@RequestMapping("text_read.inc")
-	public String text_read() {
+	public String text_read(String cPage) {
+		Map<String, String> map = new HashMap<>();
+		map.put
+		n_dao.getList(map);
 		return "text_read";
 	}
 }
