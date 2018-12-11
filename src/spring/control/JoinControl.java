6e3464bfad2cac9175c109b26a749c0d0a66ed83
thsrns593 <44997876+thsrns593@users.noreply.github.com>
2018-12-11 17:47:12 +0900

    회원가입 기능
    controller-servlet.xml, lib
    ajax쓰기 위해 수정된 부분 있습니다.

--- src/spring/control/JoinControl.java
+++ src/spring/control/JoinControl.java
@@ -1,13 +1,62 @@
 package spring.control;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import mybatis.dao.MemberDAO;
+import mybatis.vo.MemberVO;
 
 @Controller
 public class JoinControl {
-
+	
+	@Autowired MemberDAO m_dao;
+	
 	@RequestMapping("join.inc")
 	public String join() {
 		return "join";
 	}
+	@RequestMapping("reg.inc")
+	public ModelAndView reg(MemberVO vo) {
+		boolean chk = m_dao.join(vo);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:main.inc");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="checkid.inc",
+			method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, String> checkid(String id) {
+		MemberVO vo = m_dao.searchId(id);
+		Map<String, String> map = new HashMap<String, String>();
+		if(vo == null) {
+			map.put("msg", "1");
+		}else {
+			map.put("msg", "0");
+		}
+		
+		return map;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
